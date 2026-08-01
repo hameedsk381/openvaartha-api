5a6e5de82d082a072ec4ed829a1c940e3386d429
hameed <hameedsk381@gmail.com>
2026-08-01 12:36:42 +0530

    fix: sanitize document models in init_beanie (remove hallucinations, add missing tokens)

--- openvaartha-api/app/database.py
+++ openvaartha-api/app/database.py
@@ -8,12 +8,12 @@ from app.models.author import Author
 from app.models.category import Category
 from app.models.comment import Comment
 from app.models.digest import DailyDigest
-from app.models.newsletter import NewsletterSubscriber, NewsletterIssue
+from app.models.newsletter import NewsletterSubscriber
 from app.models.poll import Poll, PollVote
 from app.models.reaction import Reaction
-from app.models.reading_list import ReadingList, ReadingHistory
+from app.models.reading_list import ReadingList
 from app.models.series import Series
-from app.models.user import User
+from app.models.user import User, PasswordResetToken
 
 # MongoDB client
 client = AsyncIOMotorClient(settings.MONGODB_URL)
@@ -23,8 +23,8 @@ db = client[settings.DATABASE_NAME]
 async def init_db():
     await init_beanie(database=db, document_models=[
         Article, Source, Author, Category, Comment, DailyDigest,
-        NewsletterSubscriber, NewsletterIssue, Poll, PollVote,
-        Reaction, ReadingList, ReadingHistory, Series, User
+        NewsletterSubscriber, Poll, PollVote,
+        Reaction, ReadingList, Series, User, PasswordResetToken
     ])
 
 # Dependency
